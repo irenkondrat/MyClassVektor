79f3f2d6187f27a83601a07017379c4831daa1db
irenkondrat <irenkondrat98@mail.com>
2017-12-13 14:41:28 +0200

    added summary

--- KVector/BaseVector.cs
+++ KVector/BaseVector.cs
@@ -4,19 +4,35 @@ namespace KVector
 {
     public class BaseVector
     {
+        /// <summary>
+        /// Сoordinates of the vector
+        /// </summary>
         internal int[] Coordinates { get; set; }
 
+        /// <summary>
+        /// Constructor class BaseVector
+        /// </summary>
+        /// <param name="coordinates">coordinates of the vector</param>
         protected BaseVector(int[]coordinates)
         { 
             Coordinates = new int[coordinates.Length];
             Coordinates = coordinates;
         }
 
+        /// <summary>
+        /// Constructor class BaseVector
+        /// </summary>
+        /// <param name="countCoordinate">number of coordinates of the vector</param>
         protected BaseVector(int countCoordinate)
         {
             Coordinates = new int[countCoordinate];
         }
-
+        /// <summary>
+        /// Returns scalar product of vectors.
+        /// </summary>
+        /// <param name="vectors">vectors</param>
+        /// <param name="countCoordinate">number of coordinates of the vecto</param>
+        /// <returns></returns>
         protected static double Multiply(List<BaseVector> vectors, int countCoordinate)
         {
             double sum = 0;
@@ -32,18 +48,28 @@ namespace KVector
             }
             return sum;
         }
-
-        protected static BaseVector Multiply(BaseVector vectors, int values)
+        /// <summary>
+        /// Returns the scalar product of the vector 
+        /// </summary>
+        /// <param name="vector">vector</param>
+        /// <param name="values">scalar</param>
+        /// <returns></returns>
+        protected static BaseVector Multiply(BaseVector vector, int values)
         {
-            BaseVector resultVector = new BaseVector(vectors.Coordinates.Length);
+            BaseVector resultVector = new BaseVector(vector.Coordinates.Length);
 
-            for (var i = 0; i < vectors.Coordinates.Length; ++i)
+            for (var i = 0; i < vector.Coordinates.Length; ++i)
             {
-                resultVector.Coordinates[i] = values * vectors.Coordinates[i];
+                resultVector.Coordinates[i] = values * vector.Coordinates[i];
             }
             return resultVector;
         }
-
+        /// <summary>
+        /// Subtracts two vectors
+        /// </summary>
+        /// <param name="vectors">vectors</param>
+        /// <param name="countCoordinate">number of coordinates of the vecto</param>
+        /// <returns></returns>
         protected static BaseVector Subtraction(List<BaseVector> vectors, int countCoordinate)
         {
             BaseVector resultVector = new BaseVector(countCoordinate);
@@ -53,7 +79,12 @@ namespace KVector
             }
             return resultVector;
         }
-
+        /// <summary>
+        /// Adds two vectors
+        /// </summary>
+        /// <param name="vectors">vectors</param>
+        /// <param name="countCoordinate">number of coordinates of the vecto</param>
+        /// <returns></returns>
         protected static BaseVector Addition(List<BaseVector> vectors, int countCoordinate)
         {
             BaseVector resultVector= new BaseVector(countCoordinate);
--- KVector/Vector.cs
+++ KVector/Vector.cs
@@ -6,24 +6,35 @@ namespace KVector.Vector
 {
     public class Vector : BaseVector,IEquatable<Vector>, IFormattable
     {
+        /// <summary>
+        /// X coordinate of the 3D vector.
+        /// </summary>
         public int X
         {
             get { return Coordinates[0]; }
             set { Coordinates[0] = value; }
         }
-
+        /// <summary>
+        /// Y coordinate of the 3D vector.
+        /// </summary>
         public int Y
         {
             get { return Coordinates[1]; }
             set { Coordinates[1] = value; }
         }
 
+        /// <summary>
+        /// Z coordinate of the 3D vector.
+        /// </summary>
         public int Z
         {
             get { return Coordinates[2]; }
             set { Coordinates[2] = value; }
         }
 
+        /// <summary>
+        /// Length of the 3D vector.
+        /// </summary>
         public double Length
         {
             get
@@ -36,32 +47,61 @@ namespace KVector.Vector
                 return Math.Round(Math.Sqrt(sum), 2);
             }
         }
-
+        /// <summary>
+        /// Constructor class Vector
+        /// </summary>
         public Vector() : this(0, 0, 0)
         {
         }
-
+        /// <summary>
+        /// Constructor class Vector
+        /// </summary>
+        /// <param name="x">X coordinate of the vector</param>
+        /// <param name="y">Y coordinate of the vector</param>
+        /// <param name="z">Z coordinate of the vector</param>
         public Vector(int x, int y, int z) : base(new [] {x, y, z})
         {
         }
 
+        /// <summary>
+        /// Adds two vectors
+        /// </summary>
+        /// <param name="firstVector">First vector</param>
+        /// <param name="secondVector">Second vecrot</param>
+        /// <returns></returns>
         public static Vector operator +(Vector firstVector, Vector secondVector)
         {
             var result = Addition(new List<BaseVector>() {firstVector, secondVector}, 3);
             return new Vector(result.Coordinates[0], result.Coordinates[1], result.Coordinates[2]);
         }
-
+        /// <summary>
+        /// Subtracts two vectors
+        /// </summary>
+        /// <param name="firstVector">First vector</param>
+        /// <param name="secondVector">Second vecrot</param>
+        /// <returns></returns>
         public static Vector operator -(Vector firstVector, Vector secondVector)
         {
             var result = Subtraction(new List<BaseVector>() { firstVector, secondVector }, 3);
             return new Vector(result.Coordinates[0], result.Coordinates[1], result.Coordinates[2]);
         }
 
+        /// <summary>
+        /// Returns scalar product of vectors
+        /// </summary>
+        /// <param name="firstVector">First vector</param>
+        /// <param name="secondVector">Second vecrot</param>
+        /// <returns></returns>
         public static double operator *(Vector firstVector, Vector secondVector)
         {
             return Multiply(new List<BaseVector>(){firstVector,secondVector},3 );
         }
 
+        /// <summary>
+        /// Returns a new vector as a result of vector product of two vectors
+        /// </summary>
+        /// <param name="other">Second vecrot</param>
+        /// <returns></returns>
         public Vector CrossProduct(Vector other)
         {
             return new Vector()
@@ -75,6 +115,12 @@ namespace KVector.Vector
             };
         }
 
+        /// <summary>
+        /// Returns mixed product of three vectors
+        /// </summary>
+        /// <param name="secondVector">Second vector</param>
+        /// <param name="thirdVector">Third vector</param>
+        /// <returns></returns>
         public double TripleProduct(Vector secondVector, Vector thirdVector)
         {
             return Coordinates[0] * secondVector.Coordinates[1] * thirdVector.Coordinates[2] +
@@ -84,12 +130,20 @@ namespace KVector.Vector
                    Coordinates[1] * secondVector.Coordinates[0] * thirdVector.Coordinates[2] -
                    Coordinates[0] * secondVector.Coordinates[2] * thirdVector.Coordinates[1];
         }
-
+        /// <summary>
+        /// Returns the angle value between two vectors
+        /// </summary>
+        /// <param name="other">Second vector</param>
+        /// <returns></returns>
         public double AngleBetweenVectors(Vector other)
         {
             return Math.Round((this * other) /(Length* other.Length),2);
         }
-
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object
+        /// </summary>
+        /// <param name="other">Second vector</param>
+        /// <returns></returns>
         public bool Equals(Vector other)
         {
             if (other != null && (X == other.X && Y == other.Y && Z == other.Z))
